b455208e54eb9ca4b8d277870ddb62baaf02e5e0
ngustavin <gust4f3rn@gmail.com>
2021-01-03 23:12:27 -0300

    modal styles improvements

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,20 +8,34 @@ import Card from './components/card/index';
 import Modal from 'react-modal'
 
 
+// JSX html => JS
+// TSX html => TS
+
 import { MdAdd, MdClear } from 'react-icons/md'
 
 function App() {
 
   const customStyles = {
+    overlay: {
+      backgroundColor: 'rgba(0, 0, 0, 0.5)'
+    },
     content : {
-      top                   : '50%',
-      left                  : '50%',
-      right                 : 'auto',
-      bottom                : 'auto',
-      marginRight           : '-50%',
-      transform             : 'translate(-50%, -50%)'
+      top: '50%',
+      left: '50%',
+      right : 'auto',
+      bottom: 'auto',
+      marginRight: '-50%',
+      transform: 'translate(-50%, -50%)',
+      width: '600px',
+      height: '700px',
+      boxShadow: '0px 0px 0px 1980px rgba(0, 0, 0, 0.5)',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'space-between',
+      backgroundColor:'rgb(17,5,78)',
+      background: 'linear-gradient(333deg, rgba(17,5,78,1) 0%, rgba(50,25,142,1) 25%, rgba(91,51,224,1) 100%)',
+      
     }
-
   };
 
   const [modalIsOpen,setIsOpen] = React.useState(false);
@@ -59,28 +73,66 @@ function App() {
               style={customStyles}
               contentLabel="Example Modal"
            >
+             <div 
+             className="content-container"
+             style={{
+                 width: '100%',
+                 height: '100%',
+                 background: 'none',
+                 display: 'flex',
+                 flexDirection: 'column',
+                 justifyContent: 'space-between',
+                 alignItems: 'center',
+
+              }}>
               
-              <h2>Editando: </h2>
-              <button onClick={closeModal}>  
               <MdClear
-                  size={19}
-                  fill={'black'}
-                  style={{cursor: 'pointer'}}
-              /> </button>
-              <div></div>
-              <form>
-                  <input />
-                  <button>tab navigation</button>
-                  <button>stays</button>
-                  <button>inside</button>
-                          <button>the modal</button>
-                      </form>
+                fill={'black'}
+                size={23}
+                onClick={closeModal}
+                style={{position: 'absolute', top: '2%', left: '95%'}}
+              />
+              
+              <div
+                className="title"
+                style={{
+                  background: 'none',
+                  width: '100%',
+                  height: '100px',
+                  display: 'flex',
+                  justifyContent: 'center',
+                  alignItems: 'flex-start',
+                  
+                }}
+              >
+                <h1
+                  style={{
+                    color: 'white'
+                  }}>
+                    Adicionar nota
+                </h1>
+                
+              </div>
+
+              <div
+                className="body-container"
+                style={{
+                  background: 'none',
+                  width: '100%',
+                  height: '100%',
+                }}  
+              >
+                <form>
+                  <input type="text"/>
+                  <input type="text"/>
+                </form>
+                <input type="button" value="enviar"/>
+              </div>
+
+             </div>
+              
+              
            </Modal>     
-
-
-
-
-
 
 {/* Modal Area */}
 
